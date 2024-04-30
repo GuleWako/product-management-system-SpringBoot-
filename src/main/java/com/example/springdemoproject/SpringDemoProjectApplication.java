c8dc17fdcb1ce22e62f5c82b502cb6b5f9c91c66
GuleWako <gulumawakuma70gmail.com>
2024-04-30 09:54:28 +0300

    test

--- src/main/java/com/example/springdemoproject/SpringDemoProjectApplication.java
+++ src/main/java/com/example/springdemoproject/SpringDemoProjectApplication.java
@@ -9,7 +9,6 @@ import org.springframework.context.ApplicationContext;
 public class SpringDemoProjectApplication {
 
     public static void main(String[] args) {
-        //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
         ApplicationContext context =  SpringApplication.run(SpringDemoProjectApplication.class, args);
         Todo todo = context.getBean(Todo.class);
         todo.message();
